b39cac5c0ca495ab03d50a40faa409e30da35a64
Chris Allen <gh.84kjuqg365@gmail.com>
2022-08-26 15:41:55 -0700

    simple case works

--- rwc/src/main.rs
+++ rwc/src/main.rs
@@ -4,17 +4,56 @@
 
 use std::env::{current_exe, args};
 use std::io::Error;
+use std::fs;
 
 /// Entry point for the program.
 fn main() -> Result<(), Error> {
     let args : Vec<String> = args().collect();
-    if args.len() == 1 {
+    if args.len() == 1 { // should always be length 1 if no args given
         usage();
     } else {
-        println!("args: {:?}", args);
+        let file_path = &args[1];
+        let contents = fs::read_to_string(file_path);
+        match contents {
+            Ok(c) => {
+                let mut summary = handle_file_contents(c);
+                summary.file_name = file_path.to_owned();
+                print_summary(summary);
+            },
+            Err(e) => println!("error: {:?}", e),
+        };
+
+        //println!("contents of file:\n{}", contents);
     }
 
     Ok(())
+}
+
+#[derive(Debug)]
+struct FileSummary {
+    lines: usize,
+    words: usize,
+    chars: usize,
+    file_name: String,
+}
+
+fn print_summary(summary: FileSummary) {
+    println!("{}\t{}\t{}\t{}", summary.lines, summary.words, summary.chars, summary.file_name);
+}
+
+fn handle_file_contents(contents: String) -> FileSummary {
+    let mut fs = FileSummary { 
+        lines: 0, 
+        words: 0, 
+        chars: 0, 
+        file_name: "".to_owned()
+    };
+
+    fs.lines = contents.lines().count();
+    fs.words = contents.split_ascii_whitespace().count();
+    fs.chars = contents.len();
+
+    fs
 }
 
 /// Display usage directions. Should be the same as or very
@@ -34,4 +73,18 @@ fn get_current_exe_name() -> Option<String> {
         .to_str()?
         .to_owned()
         .into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_handle_file_contents_1() {
+        let simple_str = "this is a short bit of text".to_owned();
+        let fs = handle_file_contents(simple_str);
+        assert_eq!(fs.lines, 1);
+        assert_eq!(fs.words, 7);
+        assert_eq!(fs.chars, 27);
+    }
 }
