05dcd7bd0110f64f85a833550ec918f319a1b8bc
barbellfan <chris.c.allen@gmail.com>
2022-08-26 09:15:55 -0700

    Add rust docs

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,3 +1,10 @@
+//! Shared code for core-utils
+//! 
+//! This module provides shared code (if needed) for all of the core-utils projects.
+//! 
+//! No shared code exists yet. The add() function is just a placeholder.
+
+/// Placeholder function that just adds two usize values.
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
