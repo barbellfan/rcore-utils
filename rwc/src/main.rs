31beb09682d109a29a04adb015abc042e1ae03dd
Chris Allen <gh.84kjuqg365@gmail.com>
2022-10-22 15:33:21 -0700

    bare bones command line. add help text from wc.

--- rwc/src/main.rs
+++ rwc/src/main.rs
@@ -5,10 +5,49 @@
 use std::env::{current_exe, args};
 use std::io::Error;
 
+use clap::{Parser};
+
 mod wc; 
+
+/// Usage: wc [OPTION]... [FILE]...
+/// or:  wc [OPTION]... --files0-from=F
+/// Print newline, word, and byte counts for each FILE, and a total line if
+/// more than one FILE is specified.  A word is a non-zero-length sequence of
+/// characters delimited by white space.
+  
+/// With no FILE, or when FILE is -, read standard input.
+  
+/// The options below may be used to select which counts are printed, always in
+/// the following order: newline, word, character, byte, maximum line length.
+/// -c, --bytes            print the byte counts
+/// -m, --chars            print the character counts
+/// -l, --lines            print the newline counts
+///     --files0-from=F    read input from the files specified by
+///                          NUL-terminated names in file F;
+///                          If F is - then read names from standard input
+/// -L, --max-line-length  print the maximum display width
+/// -w, --words            print the word counts
+///     --help     display this help and exit
+///     --version  output version information and exit
+///
+/// GNU coreutils online help: <https://www.gnu.org/software/coreutils/>
+/// Full documentation at: <https://www.gnu.org/software/coreutils/wc>
+/// or available locally via: info '(coreutils) wc invocation'
+/// 
+/// This version is in Rust, and should act the same as the above.  
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(short = 'l', long)]
+    /// print the newline count
+    lines: bool,
+}
 
 /// Entry point for the program.
 fn main() -> Result<(), Error> {
+    let clap_args = Cli::parse();
+    println!("lines: {}", clap_args.lines);
+
     let args : Vec<String> = args().collect();
     if args.len() == 1 { // should always be length 1 if no args given
         usage();
