7d4a136f7110bec47bd160c64c89f37133c60873
Chris Allen <gh.84kjuqg365@gmail.com>
2022-10-22 21:39:40 -0700

    remove doc comments. add cli args doc comments don't work. they get all mooshed together. need to format it somehow.

--- rwc/src/main.rs
+++ rwc/src/main.rs
@@ -9,38 +9,31 @@ use clap::{Parser};
 
 mod wc; 
 
-/// Usage: wc [OPTION]... [FILE]...
-/// or:  wc [OPTION]... --files0-from=F
-/// Print newline, word, and byte counts for each FILE, and a total line if
-/// more than one FILE is specified.  A word is a non-zero-length sequence of
-/// characters delimited by white space.
-  
-/// With no FILE, or when FILE is -, read standard input.
-  
-/// The options below may be used to select which counts are printed, always in
-/// the following order: newline, word, character, byte, maximum line length.
-/// -c, --bytes            print the byte counts
-/// -m, --chars            print the character counts
-/// -l, --lines            print the newline counts
-///     --files0-from=F    read input from the files specified by
-///                          NUL-terminated names in file F;
-///                          If F is - then read names from standard input
-/// -L, --max-line-length  print the maximum display width
-/// -w, --words            print the word counts
-///     --help     display this help and exit
-///     --version  output version information and exit
-///
-/// GNU coreutils online help: <https://www.gnu.org/software/coreutils/>
-/// Full documentation at: <https://www.gnu.org/software/coreutils/wc>
-/// or available locally via: info '(coreutils) wc invocation'
-/// 
-/// This version is in Rust, and should act the same as the above.  
+/// thing
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
     #[arg(short = 'l', long)]
     /// print the newline count
     lines: bool,
+
+    #[arg(short = 'c', long)]
+    /// print the byte counts
+    bytes: bool,
+
+    #[arg(short = 'm', long)]
+    /// print the character counts
+    chars: bool,
+
+    #[arg(short = 'L', long = "max-line-length")]
+    /// print the maximum display width
+    max_line_length: Option<i32>,
+
+    #[arg(short = 'w', long)]
+    /// print the word counts
+    words: bool,
+
+    
 }
 
 /// Entry point for the program.
