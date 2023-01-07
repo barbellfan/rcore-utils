6f825a0b3c935feda050e614c0db8fadbf1838a4
Chris Allen <gh.84kjuqg365@gmail.com>
2023-01-07 14:18:15 -0800

    Add failing tests for char counts.

--- rwc/tests/wc_tests.rs
+++ rwc/tests/wc_tests.rs
@@ -347,12 +347,12 @@ mod test {
     /// Run wc with one big file, and one file with a single character and a bunch of empty lines.
     /// Count words only with the `-w` switch:
     /// ```
-    /// :~$  wc moby_dick.txt one_char.txt -w
+    /// :~$  wc tests/test_files/moby_dick.txt tests/test_files/one_char.txt -w
     /// ```
     /// Output from wc looks like this:
     /// ```
-    ///  215864 moby_dick.txt
-    ///       1 one_char.txt
+    ///  215864 tests/test_files/moby_dick.txt
+    ///       1 tests/test_files/one_char.txt
     ///  215865 total
     /// ```
     /// Make the output look like that.
@@ -379,12 +379,12 @@ mod test {
     /// Run wc with one big file, and one file with a really long line.
     /// Count words only with the `-w` switch:
     /// ```
-    /// :~$  wc moby_dick.txt jack.txt -w
+    /// :~$  wc tests/test_files/moby_dick.txt tests/test_files/jack.txt -w
     /// ```
     /// Output from wc looks like this:
     /// ```
-    ///  215864 moby_dick.txt
-    ///     240 jack.txt
+    ///  215864 tests/test_files/moby_dick.txt
+    ///     240 tests/test_files/jack.txt
     ///  216104 total
     /// ```
     /// Make the output look like that.
@@ -412,12 +412,12 @@ mod test {
     /// by and lots of empty lines.
     /// Count words only with the `-w` switch:
     /// ```
-    /// :~$  wc moby_dick.txt bummer_dude.txt -w
+    /// :~$  wc tests/test_files/moby_dick.txt tests/test_files/bummer_dude.txt -w
     /// ```
     /// Output from wc looks like this:
     /// ```
-    ///  215864 moby_dick.txt
-    ///     240 bummer_dude.txt
+    ///  215864 tests/test_files/moby_dick.txt
+    ///     240 tests/test_files/bummer_dude.txt
     ///  215866 total
     /// ```
     /// Make the output look like that.
@@ -445,12 +445,12 @@ mod test {
     /// by and lots of empty lines.
     /// Count bytes only with the `-c` switch:
     /// ```
-    /// :~$  wc moby_dick.txt bummer_dude.txt -c
+    /// :~$  tests/test_files/wc moby_dick.txt tests/test_files/bummer_dude.txt -c
     /// ```
     /// Output from wc looks like this:
     /// ```
-    /// 1276231 moby_dick.txt
-    ///     110 bummer_dude.txt
+    /// 1276231 tests/test_files/moby_dick.txt
+    ///     110 tests/test_files/bummer_dude.txt
     /// 1276341 total
     /// ```
     /// Make the output look like that.
@@ -474,4 +474,112 @@ mod test {
         Ok(())
     }
 
+    /// Count characters in one big file using the -m switch.
+    /// ```
+    /// :~$  wc -m tests/test_files/frankenstein.txt
+    /// ```
+    /// I'm picking this file because the byte count char counts are different.
+    /// 
+    /// Output from wc looks like this:
+    /// ```
+    /// 446616 tests/test_files/frankenstein.txt
+    /// ```
+    /// Make the output look like that.
+    #[test]
+    fn count_chars_frank() -> Result<(), Box<dyn std::error::Error>> {
+        let expected = "446616 tests/test_files/frankenstein.txt\n";
+
+        let mut cmd = get_cmd();
+        cmd.arg("tests/test_files/frankenstein.txt")
+            .arg("-m")
+            .assert()
+            .success()
+            .code(predicate::eq(0))
+            .stdout(predicate::eq(expected));
+        
+        Ok(())
+    }
+
+    /// Count characters in two big files using the -m switch.
+    /// ```
+    /// :~$  wc -m tests/test_files/frankenstein.txt tests/test_files/dracula.txt
+    /// ```
+    /// I'm picking these files because the byte and char counts are different.
+    /// 
+    /// Output from wc looks like this:
+    /// ```
+    ///  446616 tests/test_files/frankenstein.txt
+    ///  881118 tests/test_files/dracula.txt
+    /// 1327734 total
+    /// ```
+    /// Make the output look like that.
+    #[test]
+    fn count_chars_frank_n_drac() -> Result<(), Box<dyn std::error::Error>> {
+        let expected = concat!(
+            " 446616 tests/test_files/frankenstein.txt\n",
+            " 881118 tests/test_files/dracula.txt\n",
+            "1327734 total\n"
+        );
+
+        let mut cmd = get_cmd();
+        cmd.arg("tests/test_files/frank enstein.txt")
+            .arg("tests/test_files/dracula.txt")
+            .arg("-m")
+            .assert()
+            .success()
+            .code(predicate::eq(0))
+            .stdout(predicate::eq(expected));
+        
+        Ok(())
+    }
+
+    /// Count characters in a small file where the byte and char counts
+    /// are the same.
+    /// ```
+    /// :~$  wc -mc tests/test_files/so_tired_blues.txt
+    /// ```
+    /// Output from wc looks like this:
+    /// ```
+    /// 131 131 tests/test_files/so_tired_blues.txt
+    /// ```
+    /// Make the output look like that.
+    #[test]
+    fn chars_and_byte_count_same() -> Result<(), Box<dyn std::error::Error>> {
+        let expected = "131 131 tests/test_files/so_tired_blues.txt\n";
+
+        let mut cmd = get_cmd();
+        cmd.arg("tests/test_files/so_tired_blues.txt")
+            .arg("-mc")
+            .assert()
+            .success()
+            .code(predicate::eq(0))
+            .stdout(predicate::eq(expected));
+
+        Ok(())
+    }
+
+    /// Count characters in a large file where the byte and char counts
+    /// are different.
+    /// ```
+    /// :~$  wc -mc tests/test_files/dracula.txt
+    /// ```
+    /// Output from wc looks like this:
+    /// ```
+    /// 881118 881220 tests/test_files/dracula.txt
+    /// ```
+    /// Make the output look like that.
+    #[test]
+    fn chars_and_byte_count_diff() -> Result<(), Box<dyn std::error::Error>> {
+        let expected = "881118 881220 dracula.txt\n";
+
+        let mut cmd = get_cmd();
+        cmd.arg("tests/test_files/dracula.txt")
+            .arg("-mc")
+            .assert()
+            .success()
+            .code(predicate::eq(0))
+            .stdout(predicate::eq(expected));
+
+        Ok(())
+    }
 }
